72ba9301b47af55c90158eba231cb5a76c9b5e9b
muddiman <muddiman@hotmail.com>
2018-10-26 08:24:32 -0400

    minor changes

--- src/script.js
+++ src/script.js
@@ -1,45 +1,64 @@
+/*
+=========================================================================
+|                                                                       |
+|                           EINSTEIN                                    |
+|                       BY: .muddicode                                  |
+|                                                                       |
+=========================================================================
+*/
+
 
 function getAnswer() {
-    return 35;
+    // return a random integer between 11 and 99
+    return Math.floor(Math.random() * 100);
 }
 
 function squareIt(x) {
     square = x * x;
     return square;
 }
-
+/* 
 function getUsersGuess() {
-    var users_guess = document.getElementById("users_guess_html").value;
-    return users_guess;
+    return document.getElementById("user_input").value;
 }
-
+ */
+/* 
 function isCorrect(answer, guess) {
+    // compare user input with original number
     if (answer == guess) {
         return true;
     } else {
         return false;
     }
 }
-
+ */
 function answer() {
-    var answer = getAnswer();
-    if (isCorrect(answer, getUsersGuess())) {
-        ctx.fillText("1111", 30, 350);
+    // display win or lose
+    var c = document.getElementById("gameCanvas");
+    var ctx = c.getContext("2d");
+    var canvasWidth = 800;
+    var canvasHeight = 500;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.font = "bold 200px Arial";
+    if (getAnswer() == document.getElementById("user_input").value) {
+        ctx.fillText("Correct!", 30, 350);
     } else {
-        ctx.fillText("0000", 30, 350);
+        ctx.fillText("Wrong!", 30, 350);
     } 
 }
 // The game loop ESC = Quit()
 
 // The main program
 
-function main() {
+function question() {
+    var canvasWidth = 800;
+    var canvasHeight = 500;
     var c = document.getElementById("gameCanvas");
     var ctx = c.getContext("2d");
+    var the_answer = getAnswer(); 
+
     ctx.font = "bold 300px Arial";
-    ctx.fillText("1225", 30, 350);
-    // var x = document.forms["myForm"]["number"].value;
-        
-    
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillText(squareIt(the_answer), 30, 350);       
 
 }
